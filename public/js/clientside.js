10569b11a8dd9d3e9e8fd96cd1a9371b738a720e
Gautham Badhrinathan <b.gautham@gmail.com>
2012-04-07 19:28:09 +0400

    UI for Login.

--- public/js/clientside.js
+++ public/js/clientside.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
 	$('#login-form').dialog({
 		autoOpen: false,
+		show: 'fade',
+		hide: 'fade',
 		height: 240,
 		width: 400,
 		modal: true,
 		resizable: false,
 		buttons: {
 			"Login": function() {},
-			"Cancel": function() {}
+			"Cancel": function() {$('#login-form').dialog('close')}
 		}
 	});
+	$('#login-button').button().click(function() {$('#login-form').dialog('open')});
 	
 	$('#ranking-box').hover(function() {$(this).addClass('ui-state-hover')}, function() {$(this).removeClass('ui-state-hover')});
 	$('#ranking-box').mousedown(function() {$(this).addClass('ui-state-focus')});
 	$('#ranking-box').mouseup(function() {$(this).removeClass('ui-state-focus')});
+	
+	$('#problems-contents div').hover(function() {$(this).addClass('ui-state-hover')}, function() {$(this).removeClass('ui-state-hover')});
+	$('#problems-contents div').mousedown(function() {$(this).addClass('ui-state-focus')});
+	$('#problems-contents div').mouseup(function() {$(this).removeClass('ui-state-focus')});
+	$('#problems-contents div:last-child').addClass('ui-corner-bottom');
 });
